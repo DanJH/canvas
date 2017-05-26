6a4bac3b66e81006d07c87783f00c225b50e4290
Daniel Heidorn <daniel.j.heidorn@gmail.com>
2017-05-26 11:33:33 -0400

    Added random positioning

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,30 @@
 var canvas = document.getElementById('mainCanvas');
 var ctx = canvas.getContext('2d');
+var w = canvas.width;
+var h = canvas.height;
+
+var obj = [
+    {
+        name: "square",
+        pos: [10, 10],
+        size: [100, 100]
+    }
+];
 
 $(document).keypress(function(event) {
     switch (event.which) {
-        case "w":
-            //left
-        case "s":
-        case "a":
-        
+        case "c":
+          //  pos.random
+
     }
 });
 
-function draw(e) {   
-    var pos = getMousePos(canvas, e);
-    posx = pos.x;
-    posy = pos.y;
-
+function draw() {
+    ctx.clearRect(0, 0, w, h);
+    
     ctx.fillStyle = "#000000";
-    ctx.fillRect(posx, posy, 4, 4);
-} 
+    for (var i in obj) {
+        ctx.fillRect(obj[i].pos[0], obj[i].pos[1], obj[i].size[0], obj[i].size[1]);
+    }
+}
 setInterval(draw, 50/3); //1000/fps
